cd9df34d5e761c87f4f3cd5af35d55d3030773b0
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-14 17:59:15 +0400

    coke machine

---
+++ coke.py
@@ -0,0 +1,25 @@
+# print the proce of Coke
+# insert coin, where accepts only 25; 10 and 5 coins.
+# if insert coin is 50 no amount owed.
+# if you insert 25, 10 or 5 five calculation how much do you owed 
+# ignore any integer that isn’t an accepted denomination
+
+amount_due = 50
+print("Amount Due: ", amount_due)
+
+
+
+while amount_due > 0:
+    insert_coin = int(input("Insert Coin: "))
+    match insert_coin:
+        case 25 | 10 | 5:
+            amount_due -= insert_coin 
+            print(amount_due)
+            continue
+        case _:
+            continue
+print(amount_due)
+
+    
+
+
---
+++ t.py
@@ -0,0 +1,4 @@
+x = int(input("Enter Number: "))
+
+if x == [5,10,15]:
+    print("Nagdia")
