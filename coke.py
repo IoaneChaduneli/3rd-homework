f1e516194bfe68558f5ea6db7abf320262c9dc7a
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-14 18:03:18 +0400

    comment

--- coke.py
+++ coke.py
@@ -1,9 +1,5 @@
-# print the proce of Coke
-# insert coin, where accepts only 25; 10 and 5 coins.
-# if insert coin is 50 no amount owed.
-# if you insert 25, 10 or 5 five calculation how much do you owed 
-# ignore any integer that isn’t an accepted denomination
 
+# print the proce of Coke
 amount_due = 50
 print("Amount Due: ", amount_due)
 
@@ -12,10 +8,13 @@ print("Amount Due: ", amount_due)
 while amount_due > 0:
     insert_coin = int(input("Insert Coin: "))
     match insert_coin:
+# insert coin, where accepts only 25; 10 and 5 coins.
         case 25 | 10 | 5:
             amount_due -= insert_coin 
+            # if you insert 25, 10 or 5 five calculation how much do you owed 
             print("Amount Due: ",amount_due)
             continue
+        # ignore any integer that isn’t an accepted denomination
         case _:
             continue
 print("Amount Due: ",amount_due)
